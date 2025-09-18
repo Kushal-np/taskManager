0d981831f0c6ba03a71eebca0cfa4a7d0ac8cd3c
Kushal-np <wrckushal@gmail.com>
2025-09-18 22:59:59 +0545

    Done with the controller

---
+++ Server/models/task.model.js
@@ -0,0 +1,40 @@
+import {v4 as uuidv4} from 'uuid';
+
+let tasks = [];
+
+export function getAllTasks(){
+    return tasks;
+}
+
+export function createTask({title,description = '' , status='todo'}){
+    const newTask ={
+        id:uuidv4(),
+        title,
+        description,
+        status,
+        createdAt: new Date().toISOString(),
+    };
+    tasks.push(newTask);
+    return newTask;
+}
+
+export function updateTask(id,updates){
+    let found = false , 
+    tasks = tasks.map(t => {
+        if(t.id === id ){
+            found = true;
+            return {
+                ...t , 
+                ...updates
+            }
+        }
+        return t ; 
+    });
+    return found ; 
+}
+
+export function deleteTask(id){
+    const before = tasks.length;
+    tasks = tasks.filter(t=>t.id !== id);
+    return tasks.length < before ;
+}
