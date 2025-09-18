75754cc24146f7f9d3d1cc5b4f55051c7bb6228b
Kushal-np <wrckushal@gmail.com>
2025-09-18 22:22:44 +0545

    added terminal to the default

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv' ; 
 import cors from 'cors';
-
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
-
+app.use((err,req,res,next) =>{
+    console.log(err) ; 
+    res.status(500).json({
+        error:"Something went wrong",
+    })
+})
 app.get("/"  , (req,res) =>{
     res.send("Hello world");
 })
 
 app.listen(PORT , () => {
-    console.log("Hello world")
+    console.log("Hello world" , PORT);
 })
